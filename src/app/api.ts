b8981e194a6f54ee0495cff305ab10b9883fd491
Volker Peters <volker.peters@fela.ch>
2025-12-01 15:39:59 +0100

    Add simple demo application

---
+++ src/app/api.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Api {
+
+  constructor(private http: HttpClient) {}
+
+  getHello(): Observable<{ message: string }> {
+    // /api is routed to Node via the proxy config
+    return this.http.get<{ message: string }>('/api/hello');
+  }
+}
