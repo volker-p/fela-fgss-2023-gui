a5ee86b63dbff8d844a5974379dfe93f3cccfbbe
Volker Peters <volker.peters@fela.ch>
2025-12-01 15:39:59 +0100

    Add simple demo application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Api } from './api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class App {
-  protected readonly title = signal('client');
+  message = '';
+  inputText = '';
+  response = '';
+
+  constructor(private api: Api) {}
+
+  // Call backend
+  sayHello() {
+    this.api.getHello().subscribe({
+      next: (res: { message: string; }) => this.message = res.message,
+      error: () => this.message = 'Error: Could not reach backend'
+    });
+  }
+
+  // Simple demo function
+  echoInput() {
+    this.response = `You typed: ${this.inputText}`;
+  }
 }
